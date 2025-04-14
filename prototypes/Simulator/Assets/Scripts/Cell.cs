9c8f7f411a09d2fecbdd7f5f4eb4079cd4658019
LeeDaYeon <57980181+ErigoLee@users.noreply.github.com>
2025-04-14 00:09:08 -0400

    init commit 16

--- prototypes/Simulator/Assets/Scripts/Cell.cs
+++ prototypes/Simulator/Assets/Scripts/Cell.cs
@@ -4,8 +4,12 @@ using TMPro;
 public class Cell : MonoBehaviour
 {
     private GameObject keyPrefab;  // Reference to the key prefab
+    private GameObject actualKeyPrefab;
+    private bool isObj;
 
-    
+    void Start(){
+        isObj = false;
+    }
     public void SetKeyPrefab(GameObject keyObj){
         if (keyObj != null){
             keyPrefab = keyObj;
@@ -18,6 +22,25 @@ public class Cell : MonoBehaviour
         Debug.Log("Cell clicked: " + gameObject.name);
         // Instantiate the key prefab on the clicked cell's position
         Quaternion rotation = Quaternion.Euler(0, 0, 90f);
-        Instantiate(keyPrefab, transform.position + Vector3.up * 1.5f, rotation);  // Offset to position above the cell
+        actualKeyPrefab = Instantiate(keyPrefab, transform.position + Vector3.up * 1.5f, rotation);  // Offset to position above the cell
+        isObj = true;
+    }
+
+    public GameObject GettingGameObj(){
+        return actualKeyPrefab;
+    }
+
+    public bool GettingIsObj(){
+        return isObj;
+    }
+
+    public void RemoveObj(){
+        if (actualKeyPrefab != null){
+            Destroy(actualKeyPrefab);
+        }
+    }
+
+    public void SettingIsObj(){
+        isObj = false;
     }
 }
--- prototypes/Simulator/Assets/Scripts/GridManager_2.cs
+++ prototypes/Simulator/Assets/Scripts/GridManager_2.cs
@@ -1,4 +1,7 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
 using TMPro;
 
 public class GridManager_2 : MonoBehaviour
@@ -10,10 +13,45 @@ public class GridManager_2 : MonoBehaviour
     private int gridSize = 10;
     private float cellSpacing = 1f;
     private  Cell [,] cells;
+
+    //Buttons
+    [SerializeField] private Button coffeeBut;
+    [SerializeField] private Button bookBut;
+    [SerializeField] private Button movieBut;
+    [SerializeField] private Button houseBut;
+    [SerializeField] private Button deleteBut;
+    [SerializeField] private Button startBut;
+    [SerializeField] private Button resetBut;
+
+    //prefabs
+    [SerializeField] private GameObject coffee;
+    [SerializeField] private GameObject book;
+    [SerializeField] private GameObject movie;
+    [SerializeField] private GameObject house;
+
+
+    private string nameObj;
+
+    private bool startChecking;
+
+    private List<Cell> cellsObjs;
     void Start()
     {
         cells = new Cell[gridSize, gridSize];
         CreateGrid();
+
+        //addListener
+        coffeeBut.onClick.AddListener(OnButtonClickCoffee);
+        bookBut.onClick.AddListener(OnButtonClickBook);
+        movieBut.onClick.AddListener(OnButtonClickMovie);
+        houseBut.onClick.AddListener(OnButtonClickHouse);
+        deleteBut.onClick.AddListener(OnButtonClickDelete);
+        startBut.onClick.AddListener(OnButtonClickStart);
+        resetBut.onClick.AddListener(OnButtonClickReset);
+        nameObj = "";
+        startChecking = false;
+
+        cellsObjs = new List<Cell>();
     }
 
     // Create a 10x10 grid of cellPrefabs
@@ -27,11 +65,13 @@ public class GridManager_2 : MonoBehaviour
                 GameObject cell = Instantiate(cellPrefab, position, Quaternion.identity, transform);
                 // Add the Cell script to handle click events for this cell
                 Cell cellScript = cell.AddComponent<Cell>();
-                cellScript.SetKeyPrefab(keyPrefab);  // Assign the key prefab to the cell
-
                 // Optionally, name the cells for easier identification
                 cell.name = "Cell_" + x + "_" + y;
                 cells[x,y] = cellScript;
+
+
+                
+                
             }
         }
     }
@@ -40,26 +80,110 @@ public class GridManager_2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) // Left mouse button click
-        {
-            Vector3 mousePosition = Input.mousePosition;
-            mousePosition.z = 10f; // Distance from the camera, adjust as necessary
-
-            // Convert the mouse position to world space
-            Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-
-            // Raycast directly to the point where the mouse is clicking
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit))
+        if(startChecking){
+            if (Input.GetMouseButtonDown(0)) // Left mouse button click
             {
-                Vector3 hitPoint = hit.point;
-                Vector2Int gridCoord = new Vector2Int(Mathf.RoundToInt(hitPoint.x / cellSpacing), Mathf.RoundToInt(hitPoint.z / cellSpacing));
-                Debug.Log("Cell clicked at: " + gridCoord);
-                tMP_Text.text = gridCoord.ToString();
-                cells[gridCoord.x,gridCoord.y].ClickingKeyMaking();
+                Vector3 mousePosition = Input.mousePosition;
+                mousePosition.z = 10f; // Distance from the camera, adjust as necessary
+
+                // Convert the mouse position to world space
+                Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+                // Raycast directly to the point where the mouse is clicking
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    Vector3 hitPoint = hit.point;
+                    Vector2Int gridCoord = new Vector2Int(Mathf.RoundToInt(hitPoint.x / cellSpacing), Mathf.RoundToInt(hitPoint.z / cellSpacing));
+                    Debug.Log("Cell clicked at: " + gridCoord);
+                    tMP_Text.text = gridCoord.ToString();
+                    if(gridCoord.x >= gridSize || gridCoord.y >= gridSize)
+                     return;
+
+                    if(nameObj.Equals("coffee")){//nameObj == "coffee"
+                        print("coffee2");
+                        Cell cellScript = cells[gridCoord.x,gridCoord.y];
+                        //cells[gridCoord.x,gridCoord.y].ClickingKeyMaking();
+                        if(cellScript.GettingIsObj()==false){
+                            print("coffee3");
+                            cellScript.SetKeyPrefab(coffee);  // Assign the key prefab to the cell
+                            cellsObjs.Add(cellScript);
+                            cellScript.ClickingKeyMaking();
+                        }
+                    }
+
+                    if(nameObj.Equals("book")){
+                        Cell cellScript = cells[gridCoord.x,gridCoord.y];
+                        if(cellScript.GettingIsObj()==false){
+                            cellScript.SetKeyPrefab(book);  // Assign the key prefab to the cell
+                            cellsObjs.Add(cellScript);
+                            cellScript.ClickingKeyMaking();
+                        }
+                    
+                    }
+
+                    if(nameObj.Equals("movie")){
+                        Cell cellScript = cells[gridCoord.x,gridCoord.y];
+                        if(cellScript.GettingIsObj()==false){
+                            cellScript.SetKeyPrefab(movie);  // Assign the key prefab to the cell
+                            cellsObjs.Add(cellScript);
+                            cellScript.ClickingKeyMaking();
+                        }
+                    
+                    }
+                    if(nameObj.Equals("house")){
+                        Cell cellScript = cells[gridCoord.x,gridCoord.y];
+                        if(cellScript.GettingIsObj()==false){
+                            cellScript.SetKeyPrefab(house);  // Assign the key prefab to the cell
+                            cellsObjs.Add(cellScript);
+                            cellScript.ClickingKeyMaking();
+                        }
+                    
+                     }
+
+
+                    
+                }
             }
         }
+        
+    }
+
+
+    void OnButtonClickCoffee(){
+        nameObj = "coffee";
+        print("coffee");
+    }
+
+    void OnButtonClickBook(){
+        nameObj = "book";
+    }
+
+    void OnButtonClickMovie(){
+        nameObj = "movie";
+    }
+    void OnButtonClickHouse(){
+        nameObj = "house";
+    }
+
+    void OnButtonClickDelete(){
+        nameObj = "";
+    }
+
+    void OnButtonClickStart(){
+        startChecking = true;
+    }
+
+    void OnButtonClickReset(){
+        startChecking = false;
+       
+        int size = cellsObjs.Count;
+        for(int i=0;i<size;i++){
+            cellsObjs[i].SettingIsObj();
+            cellsObjs[i].RemoveObj();
+        }
+        cellsObjs.Clear();
     }
 }
